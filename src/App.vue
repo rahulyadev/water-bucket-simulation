c05060ddc04a9ad77efed9938dd90c8a5c9e8348
Rahul Yadav <rahul.yadav@gainfront.com>
2024-01-30 12:34:23 +0530

    Implemented the basic requirement.

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,153 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="tanks">
+    <Tank
+      v-for="(tank, index) in tanks"
+      :key="index"
+      :level="tank.level"
+      :buffer="tank.buffer"
+      @empty-tank="emptyTank(index)"
+      @start-filling="startFilling(index)"
+      @stop-filling="stopFilling(index)"
+    />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+<script setup>
+import { reactive, onUnmounted } from "vue";
+import Tank from "./components/Tank.vue";
+
+const tanks = reactive([
+  { id: 1, level: 0, buffer: 0, addIntervalId: null },
+  { id: 2, level: 0, buffer: 0, addIntervalId: null },
+  { id: 3, level: 0, buffer: 0, addIntervalId: null },
+  { id: 4, level: 0, buffer: 0, addIntervalId: null },
+]);
+
+let addWaterToTanksInterval = setInterval(() => distributeWater(tanks), 50);
+
+const startFilling = (index) => {
+  console.log("startFilling", index, tanks);
+  if (!addWaterToTanksInterval) {
+    addWaterToTanksInterval = setInterval(() => distributeWater(tanks), 50);
+  }
+  const tank = tanks[index];
+  console.log("startFilling tank", tank);
+  if (!tank.addIntervalId) {
+    tank.addIntervalId = setInterval(() => {
+      const intervalSeconds = 20 / 1000;
+      const inflowRatePerSecond = 20;
+      const addAmount = (inflowRatePerSecond * intervalSeconds) / 100;
+      tank.buffer = tank.buffer + addAmount * 100;
+    }, 20);
+  }
+};
+
+const stopFilling = (index) => {
+  console.log("stopFilling", index, tanks);
+  const tank = tanks[index];
+  if (tank.addIntervalId) {
+    console.log("stopFilling tank", tank);
+    clearInterval(tank.addIntervalId);
+    tank.addIntervalId = null;
+  }
+};
+
+const emptyTank = (index) => {
+  tanks[index].level = 0;
+  if (!addWaterToTanksInterval) {
+    addWaterToTanksInterval = setInterval(() => distributeWater(tanks), 50);
+  }
+};
+
+// onMounted(() => {
+// });
+
+onUnmounted(() => {
+  // if (distributionInterval) {
+  //   clearInterval(distributionInterval);
+  // }
+  if (addWaterToTanksInterval) {
+    clearInterval(addWaterToTanksInterval);
+    addWaterToTanksInterval = null;
+  }
+});
+
+// const addToTank = (tanks) => {
+//   const intervalSeconds = 10 / 1000;
+//   const inflowRatePerSecond = 2.5;
+//   const addAmount = (100 * inflowRatePerSecond * intervalSeconds) / 100;
+//   tanks.forEach((tank) => {
+//     if (tank.buffer > 0 && tank.level < 100 - addAmount) {
+//       console.log("addToTank", tank);
+//       const finalAddAmount = Math.min(tank.buffer, addAmount);
+//       tank.level = Math.min(tank.level + finalAddAmount, 100);
+//       tank.buffer -= finalAddAmount;
+//     }
+//   });
+//   distributeWater(tanks);
+// };
+
+const distributeWater = (tanks) => {
+  const intervalSeconds = 50 / 1000;
+  const outflowRatePerSecond = 2.5;
+  const flowAmount = (100 * (outflowRatePerSecond * intervalSeconds)) / 100;
+  console.log("flowAmount", flowAmount);
+
+  tanks.forEach((tank) => {
+    if (tank.buffer > 0 && tank.level < 100 - flowAmount) {
+      console.log("addToTank", tank);
+      const finalAddAmount = Math.min(tank.buffer, flowAmount);
+      tank.level = Math.min(tank.level + finalAddAmount, 100);
+      tank.buffer -= finalAddAmount;
+    }
+  });
+
+  let totalWater = tanks.reduce((acc, tank) => acc + tank.level, 0);
+  console.log("totalWater", totalWater);
+
+  const averageLevel = totalWater / tanks.length;
+  console.log("averageLevel", averageLevel);
+  const isEquilibrium = tanks.every(
+    (tank) => Math.abs(tank.level - averageLevel) < 0.0001
+  );
+  if (isEquilibrium) {
+    console.log("Equilibrium reached:", tanks);
+    clearInterval(addWaterToTanksInterval);
+    addWaterToTanksInterval = null;
+    return;
+  }
+
+  let totalOutflow = 0;
+  tanks.forEach((tank) => {
+    if (tank.level > averageLevel) {
+      totalOutflow += flowAmount;
+      tank.level -= flowAmount;
+    }
+  });
+
+  const totalDeficit = tanks.reduce((acc, tank) => {
+    return tank.level < averageLevel ? acc + (averageLevel - tank.level) : acc;
+  }, 0);
+  tanks.forEach((tank) => {
+    if (tank.level < averageLevel) {
+      const deficitProportion = (averageLevel - tank.level) / totalDeficit;
+      console.log("deficitProportion", tank.id, deficitProportion);
+      const inflow = totalOutflow * deficitProportion;
+      console.log("inflow", tank.id, inflow);
+      tank.level += inflow;
+    }
+  });
+};
+
+// const distributionInterval = setInterval(() => distributeWater(tanks), 200);
+
+</script>
+
+<style>
+.tanks {
+  display: flex;
+  min-width: fit-content;
+  justify-content: center;
+  margin: 20px auto;
 }
 </style>
---
+++ src/components/Tank.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="container">
+    <div class="buttons">
+      <button
+        @mousedown="startFilling"
+        @mouseup="stopFilling"
+        @mouseleave="stopFilling"
+        @touchstart="startFilling"
+        @touchend="stopFilling"
+      >
+        Add water
+      </button>
+      <button @click="emitEmpty">Empty tank</button>
+    </div>
+    <div class="buffer">Water in buffer: {{ (buffer * 10).toFixed(1) }}L</div>
+    <div class="tank">
+      <div class="water" :style="{ height: waterHeight + 'px' }"></div>
+    </div>
+    Capacity: 1000L
+    <br />
+    Water filled: {{ (level * 10).toFixed(3) }}L
+  </div>
+</template>
+
+<script setup>
+import { computed, toRefs } from "vue";
+const props = defineProps({
+  level: Number,
+  buffer: Number,
+});
+const emit = defineEmits(['start-filling', 'stop-filling', 'empty-tank']);
+const { level, buffer } = toRefs(props);
+
+const waterHeight = computed(() => {
+  const tankHeight = 200;
+  return (level.value / 100) * tankHeight;
+});
+
+const startFilling = () => {
+  emit("start-filling");
+};
+
+const stopFilling = () => {
+  emit("stop-filling");
+};
+
+const emitEmpty = () => {
+  emit("empty-tank");
+};
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  margin: 5px 5px;
+}
+.buttons {
+  display: flex;
+  justify-content: space-between;
+  margin: 10px 0;
+}
+
+button {
+  width: 50px;
+  margin: 0 5px;
+}
+
+.tank {
+  width: 175px;
+  height: 200px;
+  border: 2px solid #000;
+  border-top: none;
+  border-radius: 5px;
+  position: relative;
+  background-color: #fff;
+  overflow: hidden;
+}
+
+.water {
+  background-color: #0077ff;
+  width: 100%;
+  position: absolute;
+  bottom: 0;
+  height: 0;
+  transition: max-height 20ms linear;
+}
+</style>
